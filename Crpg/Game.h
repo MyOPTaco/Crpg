92d9ff7881e298b29660ccf26c1aa6879fda715d
MyOPTaco <113459344+MyOPTaco@users.noreply.github.com>
2024-03-19 20:44:47 -0700

    Add files via upload

---
+++ Crpg/Game.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Character.h"
+#include "Functions.h"
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+
+class Game
+{
+public:
+	Game();
+	virtual ~Game();
+
+	//Operators
+
+	//Functions
+	void mainMenu();
+
+	//Accessors
+	inline bool getPlaying() const { return this->playing; }
+
+	//Modifiers
+
+private:
+	int choice;
+	bool playing;
+};
+
---
+++ Crpg/Gamr.cpp
@@ -0,0 +1,1 @@
+#include "Gamr.h"
